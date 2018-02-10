fb5ec4a47887332ba7e7e721a467a1fa71014261
Jack Buckon <jbuckon@localhost.localdomain>
2018-02-10 13:35:50 -0800

    added mongodb connection functionality

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const http = require('http')
-const port = 80
+var MongoClient = require('mongodb').MongoClient;
+var MongoUrl = "mongodb://localhost:27017/";
+const http = require('http');
+const port = 80;
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+   console.log(request.url)
+   response.end('Hello Node.js Server!')
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+   if (err) {
+      return console.log('something bad happened', err)
+   }
 
-  console.log(`server is listening on ${port}`)
+   console.log(`server is listening on ${port}`)
+   MongoClient.connect(MongoUrl, function(err, db) {
+      if(err) throw err;
+      var dbo = db.db("cryptodatacollector");
+      dbo.collection("orderbookitem").find({}, function(err, results) {
+         if(err) throw err;
+         for(var i = 0; i < results.length; i++){
+            console.log(results[i]);
+         }
+      });
+   });
 })
 //start file
